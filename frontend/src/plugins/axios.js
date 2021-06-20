a91d3246ad3c116461279dafa06bfc5fd1a9bb7c
Leonardo <leocoelho.pi@gmail.com>
2021-06-19 21:08:27 -0300

    set frontend

---
+++ frontend/src/plugins/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+let baseURL = `https://newsapi.org/v2`;
+let apiKey = process.env.VUE_APP_APIKEY;
+const instance = axios.create({
+    baseURL: baseURL,
+    timeout: 30000,
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+});
+export default instance;
