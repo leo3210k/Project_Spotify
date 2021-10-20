6b36df1ce1da636a6585523b79f4b419cf338ad3
Leonardo <leocoelho.pi@gmail.com>
2021-10-20 20:26:34 -0300

    add comment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
-knex.migrate.latest([config])
+knex.migrate.latest([config]) // Chamar as migrações
 module.exports = knex // Para acessar o knex diretamente no arquivo index
