18367a3e2da222f3520bbd0f9dec18982b22707d
Leonardo <leocoelho.pi@gmail.com>
2021-10-20 20:25:15 -0300

    fix bug in cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -3,5 +3,5 @@ const cors = require('cors')
 
 module.exports = app => {
     // app.use(bodyParser.json()) // vai interpretar o json q vai vir no corpo da requisição
-    app.use(cors)
+    app.use(cors())
 }
