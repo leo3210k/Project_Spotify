64e99f002e4d9c2ff6c85184e92cdc5c67e2d11a
Leonardo <leocoelho.pi@gmail.com>
2021-11-16 20:22:55 -0300

    add admin filter

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
@@ -5,11 +7,12 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
+        .delete(admin(app.api.user.remove))
 }
