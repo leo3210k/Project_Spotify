8525d2e0d18086269f190f1d7f3001b7b8f4471b
Leonardo <leocoelho.pi@gmail.com>
2021-12-05 23:30:16 -0300

    add font-awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import App from './App.vue'
+import './config/axios'
+import './config/msgs'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
  
 import router from './config/router' 
-
-Vue.config.productionTip = false
 
 new Vue({
   vuetify,
